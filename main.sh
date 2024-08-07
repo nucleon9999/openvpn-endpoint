e7d1b2768c1f683dd366a8b81e45374f05f5307b
nucleon9999 <177419046+nucleon9999@users.noreply.github.com>
2024-08-07 19:11:51 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# DO NOT KNOW IF THIS VERSION IS WORKING
+
 set -e
 
 if [[ $EUID -ne 0 ]]; then
@@ -17,34 +19,34 @@ apt-get install -qy openvpn curl iptables-persistent
 cd /etc/openvpn
 
 # authority
->ca-key.pem      openssl genrsa 2048
->ca-csr.pem      openssl req -sha256 -new -key ca-key.pem -subj /CN=OpenVPN-CA/
->ca-cert.pem     openssl x509 -req -sha256 -in ca-csr.pem -signkey ca-key.pem -days 365
->ca-cert.srl     echo 01
+openssl genpkey -algorithm RSA -out ca-key.pem -pkeyopt rsa_keygen_bits:2048
+openssl req -sha256 -new -key ca-key.pem -out ca-csr.pem -subj /CN=OpenVPN-CA/
+openssl x509 -req -sha256 -in ca-csr.pem -signkey ca-key.pem -days 365 -out ca-cert.pem
+echo 01 > ca-cert.srl
 
 # server
->server-key.pem  openssl genrsa 2048
->server-csr.pem  openssl req -sha256 -new -key server-key.pem -subj /CN=OpenVPN-Server/
->server-cert.pem openssl x509 -sha256 -req -in server-csr.pem -CA ca-cert.pem -CAkey ca-key.pem -days 365
+openssl genpkey -algorithm RSA -out server-key.pem -pkeyopt rsa_keygen_bits:2048
+openssl req -sha256 -new -key server-key.pem -out server-csr.pem -subj /CN=OpenVPN-Server/
+openssl x509 -sha256 -req -in server-csr.pem -CA ca-cert.pem -CAkey ca-key.pem -days 365 -out server-cert.pem
 
 # client
->client-key.pem  openssl genrsa 2048
->client-csr.pem  openssl req -sha256 -new -key client-key.pem -subj /CN=OpenVPN-Client/
->client-cert.pem openssl x509 -req -sha256 -in client-csr.pem -CA ca-cert.pem -CAkey ca-key.pem -days 365
+openssl genpkey -algorithm RSA -out client-key.pem -pkeyopt rsa_keygen_bits:2048
+openssl req -sha256 -new -key client-key.pem -out client-csr.pem -subj /CN=OpenVPN-Client/
+openssl x509 -req -sha256 -in client-csr.pem -CA ca-cert.pem -CAkey ca-key.pem -days 365 -out client-cert.pem
 
->dh.pem     openssl dhparam 2048
+openssl dhparam -out dh.pem 2048
 
 chmod 600 *-key.pem
 
->>/etc/sysctl.conf echo net.ipv4.ip_forward=1
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sysctl -p
 
 iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
->/etc/iptables/rules.v4 iptables-save
+iptables-save > /etc/iptables/rules.v4
 
-SERVER_IP=$(curl -s4 canhazip.com || echo "<insert server IP here>")
+SERVER_IP=$(curl -s4 ifconfig.me || echo "<insert server IP here>")
 
->udp80.conf cat <<EOF
+cat <<EOF > udp80.conf
 server      10.8.0.0 255.255.255.0
 verb        3
 duplicate-cn
@@ -58,14 +60,7 @@ persist-tun yes
 comp-lzo    no
 push        "dhcp-option DNS 8.8.8.8"
 push        "dhcp-option DNS 8.8.4.4"
-# Normally, the following command is sufficient.
-# However, it doesn't assign a gateway when using
-# VMware guest-only networking.
-#
-# push        "redirect-gateway def1 bypass-dhcp"
-push        "redirect-gateway bypass-dhcp"
-push        "route-metric 512"
-push        "route 0.0.0.0 0.0.0.0"
+push        "redirect-gateway def1 bypass-dhcp"
 user        nobody
 group       nogroup
 proto       udp
@@ -74,8 +69,7 @@ dev         tap80
 status      openvpn-status-80.log
 EOF
 
-
->tcp443.conf cat <<EOF
+cat <<EOF > tcp443.conf
 server      10.8.0.0 255.255.255.0
 verb        3
 duplicate-cn
@@ -89,32 +83,21 @@ persist-tun yes
 comp-lzo    no
 push        "dhcp-option DNS 8.8.8.8"
 push        "dhcp-option DNS 8.8.4.4"
-
-# Normally, the following command is sufficient.
-# However, it doesn't assign a gateway when using
-# VMware guest-only networking.
-#
-# push        "redirect-gateway def1 bypass-dhcp"
-
-push        "redirect-gateway bypass-dhcp"
-push        "route-metric 512"
-push        "route 0.0.0.0 0.0.0.0"
-
+push        "redirect-gateway def1 bypass-dhcp"
 user        nobody
 group       nogroup
-
-proto       udp
-port        80
-dev         tap80
-status      openvpn-status-80.log
+proto       tcp
+port        443
+dev         tap443
+status      openvpn-status-443.log
 EOF
 
->client.ovpn cat <<EOF
+cat <<EOF > client.ovpn
 client
 nobind
 dev tap
 redirect-gateway def1 bypass-dhcp
-remote $SERVER_IP 80 tap
+remote $SERVER_IP 80 udp
 comp-lzo no
 
 <key>
@@ -128,6 +111,6 @@ $(cat ca-cert.pem)
 </ca>
 EOF
 
-service openvpn restart
+systemctl restart openvpn
 cat client.ovpn
 cd -
